1c37a901bede51a547d77ffc7a36483864d8903d
Tek0ver <thibault.foure@outlook.com>
2022-12-26 18:24:10 +0100

    add 2 test spyders

--- sandbox/scrapy/recipes_spider.py
+++ sandbox/scrapy/recipes_spider.py
@@ -11,9 +11,9 @@ class QuotesSpider(scrapy.Spider):
             yield {
                 'name': recipe.xpath('a/div/p/text()').get(),
                 'time': recipe.xpath('a/div/div/div/small/span/text()').get(),
-                'difficulty': recipe.xpath('a/div/div/div/small/span/text()').get(),
+                'difficulty': recipe.xpath('a/div/div/div/small[2]/span/text()').get(),
             }
-
+            
         next_page = response.css('ul.pagination li.next a::attr("href")').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
