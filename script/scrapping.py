d1c5d0c4bd26f008a533d538927698c62d4b3814
tom1731 <tom.desire17@gmail.com>
2023-01-07 16:08:04 +0100

    convert date column from string to datetime format

--- script/scrapping.py
+++ script/scrapping.py
@@ -3,8 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
-
-# driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
