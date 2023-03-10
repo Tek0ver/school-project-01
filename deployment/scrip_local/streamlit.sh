d184d6feecc0ec051482341cd8325a7c1795c9fc
tom1731 <tom.desire17@gmail.com>
2023-03-10 09:38:18 +0100

    fix streamlit.sh for local run

--- deployment/scrip_local/streamlit.sh
+++ deployment/scrip_local/streamlit.sh
@@ -1,5 +1,5 @@
 docker build -t streamlit:1 -f deployment/streamlit/Dockerfile .
 docker run --rm -it \
     -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-    -p 8501 \
+    -p 8501:8501 \
     streamlit:1
