34bfbe2ac9555f5bff2fa4b4b53529166e621537
tom1731 <tom.desire17@gmail.com>
2023-03-18 13:52:36 +0100

    refactor: remove password from code

--- deployment/script/function_test.py
+++ deployment/script/function_test.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("az login --service-principal --username 'b929ed1d-dc29-4f80-b3ea-fee5aaf43edf' --password 'ypH8Q~-HSGTKiMxGtDRzCF1OlgkcJMNuWuA_ObPU' --tenant 'b7b023b8-7c32-4c02-92a6-c8cdaa1d189c'")
+os.system("az login --service-principal --username $AZURE_SERVICE_PRINCIPAL_APPID --password $AZURE_SERVICE_PRINCIPAL_PASSWORD --tenant $AZURE_SERVICE_PRINCIPAL_TENANT")
 os.system("az container start --name update-db --resource-group school-project-public-rg")
