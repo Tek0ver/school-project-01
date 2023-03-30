f8c52d930991e6249b88798bc6758901800c11bd
StillDenco <119936309+StillDenco@users.noreply.github.com>
2023-03-30 09:10:46 +0200

    scrap bbc

---
+++ PYTHON/moncode.py
@@ -0,0 +1,8 @@
+# cours pour apprendre les bases de python
+
+#condition if
+age=18
+if age>=0: # si l'age est inferieur ou égale à 18 
+    print ("vous etes majeur.")
+else: print("vous etes mineur")
+
